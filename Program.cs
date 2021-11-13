04305aa881d3bf4f53d289f1ee4aa4d763e5b63c
a.arnaut <a.arnaut@tirscript.com>
2021-11-13 17:59:43 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Messenger_13._11._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
