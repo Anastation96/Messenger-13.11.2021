bb9e13ce7180c99b87525a0c51064d788710c531
a.arnaut <a.arnaut@tirscript.com>
2021-11-13 20:45:33 +0200

    Добавлена отправка и получение сообщений

--- ASPCoreServer/Controllers/Messenger.cs
+++ ASPCoreServer/Controllers/Messenger.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Messenger_13._11._2021;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,18 +14,33 @@ namespace ASPCoreServer.Controllers
     [ApiController]
     public class Messenger : ControllerBase
     {
+        static List<Message> ListOfMessages = new List<Message>();
 
         // GET api/<Messenger>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "Абракадабра" + id.ToString();
+            string OutputString = "Not Found";
+            if ((id < ListOfMessages.Count) && (id >= 0))
+            {
+                OutputString = JsonConvert.SerializeObject(ListOfMessages[id]);
+            }
+            Console.WriteLine(String.Format("Запрошено сообщение № {0} : {1}", id, OutputString));
+            return OutputString;
         }
 
         // POST api/<Messenger>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult SendMessage([FromBody] Message msg)
         {
+            if (msg == null)
+            {
+                return BadRequest();
+            }
+            ListOfMessages.Add(msg);
+            Console.WriteLine(String.Format("Всего сообщений: {0}, посланное сообщение: {1}", ListOfMessages.Count, msg));
+            //return NoContentResult();
+            return new OkResult();
         }
 
     }
